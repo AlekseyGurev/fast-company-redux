8795b6b35887e45a41cb87a43a8d321c7f2dda87
Gurev Aleksey <gurevaleksey@yandex.ru>
2022-02-08 14:22:58 +0300

    register in slice

--- src/app/store/users.js
+++ src/app/store/users.js
@@ -1,4 +1,6 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAction, createSlice } from "@reduxjs/toolkit";
+import authService from "../services/auth.service";
+import localStorageService from "../services/localStorage.service";
 import userService from "../services/user.service";
 
 const usersSlice = createSlice({
@@ -6,7 +8,9 @@ const usersSlice = createSlice({
   initialState: {
     entities: null,
     isLoading: true,
-    error: null
+    error: null,
+    auth: null,
+    isLoggedIn: false
   },
   reducers: {
     usersRequested: (state) => {
@@ -19,12 +23,31 @@ const usersSlice = createSlice({
   usersRequestFiled: (state, action) => {
       state.error = action.payload;
       state.isLoading = false;
+  },
+  authRequestedSuccess: (state, action) => {
+    state.auth = { ...action.payload, isLoggedIn: true };
   }
+  },
+  authRequestedFailed: (state, action) => {
+    state.error = action.payload;
   }
 });
 
 const { reducer: usersReducer, actions } = usersSlice;
-const { usersRequested, usersReceved, usersRequestFiled } = actions;
+const { usersRequested, usersReceved, usersRequestFiled, authRequestedSuccess, authRequestedFailed } = actions;
+const authRequested = createAction("users/authRequested");
+export const signUp = ({ email, password, ...rest }) => async (dispatch) => {
+  dispatch(authRequested());
+  try {
+    const data = await authService.register({ email, password });
+    localStorageService.setTokens(data);
+    dispatch(authRequestedSuccess({
+      userId: data.localId
+    }));
+  } catch (error) {
+    dispatch(authRequestedFailed(error.message));
+  }
+};
 
 export const loadUsersList = () => async (dispatch, getState) => {
       dispatch(usersRequested());
